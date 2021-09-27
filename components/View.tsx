5f0e8451fb420498b50b7bc329d58aa2aa0ca98a
sidik saepudin <sidiksaepudin13@gmail.com>
2021-09-28 06:53:59 +0700

    init component

---
+++ components/View.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { View as DefaultView } from "react-native";
+import { ThemeProps, useThemeColor } from "../hooks/useThemeColor";
+
+export type ViewProps = ThemeProps & DefaultView["props"];
+
+export function View(props: ViewProps) {
+  const { style, lightColor, darkColor, ...otherProps } = props;
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
+
+  return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
